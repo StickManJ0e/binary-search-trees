65de512dcce5c4da4d31144f9878ac5b4a552e2a
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-03-04 14:12:04 +1100

    Add script driver commands

--- src/driver.js
+++ src/driver.js
@@ -1,8 +1,28 @@
 import {Tree, prettyPrint} from './script';
 
-console.log('hi')
 let array = [7, 6, 2, 5, 3, 10, 1, 5, 13, 21, 18, 23, 14, 32]
 let binaryTree = Tree(array);
+
+//Balanced Test
+console.log(binaryTree.isBalanced());
+console.log(binaryTree.levelOrder());
+console.log(binaryTree.preorder());
+console.log(binaryTree.postorder());
+console.log(binaryTree.inorder());
+prettyPrint(binaryTree.root);
+
+//Unbalance tree
+binaryTree.insert(101);
+binaryTree.insert(102);
+binaryTree.insert(103);
+binaryTree.insert(104);
+console.log(binaryTree.isBalanced());
+binaryTree.rebalance();
+console.log(binaryTree.isBalanced());
+console.log(binaryTree.levelOrder());
+console.log(binaryTree.preorder());
+console.log(binaryTree.postorder());
+console.log(binaryTree.inorder());
 prettyPrint(binaryTree.root);
 
 // Testing
